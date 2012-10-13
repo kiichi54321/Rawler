57feeba2f355054a81d580f30b4c24c4f5e27aac
Takaichi Ito <takaichi.ito@gmail.com>
2012-10-14 01:13:01 +0900

    KeyValueStore追加

---
+++ Rawler/Tool/Utility/KeyValueStore.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rawler.Tool.Utility;
+
+namespace Rawler
+{
+    public class KeyValueStore : RawlerBase
+    {
+        #region テンプレ
+        /// <summary>
+        /// Clone
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <returns></returns>
+        public override RawlerBase Clone(RawlerBase parent)
+        {
+            return base.Clone<KeyValueStore>(parent);
+        }
+
+        /// <summary>
+        /// ObjectのName。表示用
+        /// </summary>
+        public override string ObjectName
+        {
+            get { return this.GetType().Name; }
+        }
+        #endregion
+
+        /// <summary>
+        /// このクラスでの実行すること。
+        /// </summary>
+        /// <param name="runChildren"></param>
+        public override void Run(bool runChildren)
+        {
+            base.Run(runChildren);
+        }
+
+        /// <summary>
+        /// 子が参照するテキスト。
+        /// </summary>
+        public override string Text
+        {
+            get
+            {
+                return base.Text;
+            }
+        }
+
+
+    }
+}
