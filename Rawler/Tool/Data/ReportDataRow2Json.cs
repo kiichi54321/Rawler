35b56abcb1ee0babb2274ec117a73f70ea06212e
Takaichi Ito <takaichi.ito@gmail.com>
2014-06-04 20:34:52 +0900

    コンソール版を追加

---
+++ Rawler/Tool/Data/ReportDataRow2Json.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rawler.Tool
+{
+    public class ReportDataRow2Json:Report
+    {
+        public override void Run(bool runChildren)
+        {
+            try
+            {
+                this.Message = this.GetAncestorRawler().OfType<Data>().First().GetCurrentDataRow().ToJson();
+                this.Visible = true;
+                base.Run(runChildren);
+            }
+            catch
+            {
+                ReportManage.ErrReport(this, "上流にDataがありません。");
+            }
+        }
+    }
+}
